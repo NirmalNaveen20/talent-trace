d6041c23e3359ff09b36eb49bd52e3b29547eed4
= <=>
2023-11-21 21:20:03 +0530

    feat: update & arrange components

--- app/index.js
+++ app/index.js
@@ -13,9 +13,32 @@ const Home = () => {
         <SafeAreaView style={{flex:1, backgroundColor: COLORS.lightWhite}}>
             <Stack.Screen
                 options={{
-                    headerStyle: {backgroundColor: COLORS.lightWhite},
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+                    ),
+                    headerTitle: ""
                 }}
             />
+            <ScrollView showsHorizontalScrollIndicator={false}>
+                <View
+                    style={{
+                        flex: 1,
+                        padding: SIZES.medium
+                    }}
+                >
+                    <Welcome
+                    
+                    />
+
+                    <Popularjobs />
+                    <Nearbyjobs />
+                </View>
+            </ScrollView>
         </SafeAreaView>
     )
 }
