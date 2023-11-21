4678313535b92e2e7f86cc32262939850044acba
= <=>
2023-11-21 10:13:25 +0530

    feat: update headerstyling

--- app/index.js
+++ app/index.js
@@ -1,10 +1,22 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, ScrollView, SafeAreaView } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+
+import { COLORS, icons, images, SIZES } from '../constants';
+
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components'
 
 const Home = () => {
+    const router = useRouter();
+
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView style={{flex:1, backgroundColor: COLORS.lightWhite}}>
+            <Stack.Screen
+                options={{
+                    headerStyle: {backgroundColor: COLORS.lightWhite},
+                }}
+            />
+        </SafeAreaView>
     )
 }
 
