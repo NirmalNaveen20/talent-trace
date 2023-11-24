3fe7ec615e8f9a1f28ead5d470daa38c0a5404f5
= <=>
2023-11-24 15:57:47 +0530

    fix: update key env

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { RAPID_API_KEY } from '@env';
 
-const useFetch = () => {
+const rapidAPIKey = RAPID_API_KEY
+
+const useFetch = (endpoint) => {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
@@ -15,7 +18,7 @@ const useFetch = () => {
           num_pages: '1'
         },
         headers: {
-          'X-RapidAPI-Key': '28bcf44e77msh0c1ad35f142297ep1628d9jsn15a9e886e02d',
+          'X-RapidAPI-Key': rapidAPIKey,
           'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
         }
       };
