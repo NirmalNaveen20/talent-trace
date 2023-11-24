193ad368f5409d2b9c660b716d141ef1f9fc23c1
= <=>
2023-11-24 14:11:11 +0530

    feat: add jobtypes

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
-import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native'
+import { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native';
 
-import styles from './welcome.style'
-import { useRouter } from 'expo-router'
-import { icons, SIZES } from '../../../constants'
+import styles from './welcome.style';
+import { useRouter } from 'expo-router';
+import { icons, SIZES } from '../../../constants';
+
+const jobTypes = ["Full-time", "Part-time", "Contractor"];
 
 const Welcome = () => {
   const router = useRouter()
@@ -33,6 +35,17 @@ const Welcome = () => {
           />
         </TouchableOpacity>
 
+        <View style={styles.tabsContainer}>
+          <FlatList
+            data={jobTypes}
+            renderItem={({ item }) => (
+              <TouchableOpacity>
+                <Text>{item}</Text>
+              </TouchableOpacity>
+            )}
+          />
+        </View>
+        
       </View>
     </View>
   )
