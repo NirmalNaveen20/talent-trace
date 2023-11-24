fce5f650df551ba2cde38c7c2a4efe145692f18b
= <=>
2023-11-24 14:49:07 +0530

    feat: update popular job component

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,12 +1,22 @@
-import { useState } from 'react';
-import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
-import styles from './popularjobs.style';
-import { useRouter } from 'expo-router';
-import { COLORS, SIZES } from '../../../constants';
-import { PopularJobCard } from '../../common/cards/popular/PopularJobCard';
+import { useState } from "react";
+import { useRouter } from "expo-router";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
+
+import styles from "./popularjobs.style";
+import { COLORS, SIZES } from "../../../constants";
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+
 
 const Popularjobs = () => {
   const router = useRouter();
+  const isLoading = false;
+  const error = false;
 
   return (
     <View style={styles.container}>
@@ -16,8 +26,25 @@ const Popularjobs = () => {
           <Text style={styles.headerBtn}>Show All</Text>
         </TouchableOpacity>
       </View>
-    </View>
-  )
-}
 
-export default Popularjobs
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size='large' color={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          <FlatList
+            data={[1,2,3,4]}
+            renderItem={({ item }) => (
+              <PopularJobCard
+                item={item}
+              />
+            )}
+          />
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Popularjobs;
