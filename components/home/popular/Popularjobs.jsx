20ff8251526f735fb386599d40916385cc54ed65
= <=>
2023-11-24 14:29:50 +0530

    feat: import and add elements

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
-import styles from './popularjobs.style'
+import { useState } from 'react';
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
+import styles from './popularjobs.style';
+import { useRouter } from 'expo-router';
+import { COLORS, SIZES } from '../../../constants';
+import { PopularJobCard } from '../../common/cards/popular/PopularJobCard';
 
 const Popularjobs = () => {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular jobs</Text>
+      </View>
     </View>
   )
 }
