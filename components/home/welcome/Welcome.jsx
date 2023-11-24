f45e4b198c4a6dd05cd85919c9b2f04ab83e5f3a
= <=>
2023-11-24 13:22:25 +0530

    feat: update welcome component

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react'
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native'
 
 import styles from './welcome.style'
+import { useRouter } from 'expo-router'
+import { icons, SIZES } from '../../../constants'
 
 const Welcome = () => {
+  const router = useRouter()
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Nirmal</Text>
+        <Text style={styles.welcomeMessage}>Find your future job here</Text>
+      </View>
     </View>
   )
 }
