aafe946184950a605720f9668be73325b951e5bd
= <=>
2023-11-24 16:03:58 +0530

    feat: getting data from api

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -4,7 +4,7 @@ import { RAPID_API_KEY } from '@env';
 
 const rapidAPIKey = RAPID_API_KEY
 
-const useFetch = (endpoint) => {
+const useFetch = (endpoint, query) => {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
@@ -12,14 +12,26 @@ const useFetch = (endpoint) => {
     const options = {
         method: 'GET',
         url: `https://jsearch.p.rapidapi.com/${endpoint}`,
-        params: {
-          query: 'Python developer in Texas, USA',
-          page: '1',
-          num_pages: '1'
-        },
+        params: { ...query },
         headers: {
           'X-RapidAPI-Key': rapidAPIKey,
           'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
         }
-      };
+    };
+    
+    const fetchData = async () => {
+        setIsLoading(true);
+
+        try {
+            const response = await axios.request(options);
+
+            setData(response.data.data);
+            setIsLoading(false);
+        } catch(error) {
+            setError(error);
+            alert('There is an error')
+        } finally {
+            setIsLoading(false);
+        }
+    }
 }
